27464f9b053a29a94bcf08f91dad85f73a9ec02d
Grandpere <lorenzo.marozzo@gmail.com>
2019-07-26 20:41:34 +0200

    first version of php-p of php-phantomjs webservice

---
+++ README.md
@@ -0,0 +1,11 @@
+# PHP PhantomJS - Test screenshot as a webservice
+
+## Installation
+* Clone repository
+* move to repository folder
+* download apache server ([get Apache](https://httpd.apache.org/download.cgi))
+* ```composer install``` ([get Composer](https://getcomposer.org/))
+
+## Utilization
+* go to the browser for testing service
+* URL Example ```http://localhost/task/screenshotservice.php?id=Bzm9S8QIgD1```
---
+++ screenshotservice.php
@@ -0,0 +1,41 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use JonnyW\PhantomJs\Client;
+use JonnyW\PhantomJs\DependencyInjection\ServiceContainer;
+
+if(!isset($_GET['id'])) {
+    echo 'id not found in url';
+}
+else {
+    $id = $_GET['id'];
+    $postUrl = 'https://www.instagram.com/p/'.$id.'/';
+
+    $location = 'custom_scripts';
+    $serviceContainer = ServiceContainer::getInstance();
+    $procedureLoader = $serviceContainer->get('procedure_loader_factory')
+        ->createProcedureLoader($location);
+
+    $client = Client::getInstance();
+    $client->getProcedureCompiler()->clearCache();
+    $client->getProcedureLoader()->addLoader($procedureLoader);
+    $width  = 1440;
+    $height = 900;
+
+    $request = $client->getMessageFactory()->createCaptureRequest($postUrl, 'GET', 2000);
+    $file = 'screenshots/'.$id.'.jpg';
+    $request->setOutputFile($file);
+    $request->setViewportSize($width, $height);
+    $request->setCaptureDimensions($width, $height);
+
+    $response = $client->getMessageFactory()->createResponse();
+    $client->send($request, $response);
+
+    if($response->getStatus() != 200) {
+        echo "Unable to load the address!";
+    }
+    else {
+        echo '<img src="'.$file.'" alt="capture d\'écran du post'.$id.'">';
+    }
+}
